6e2a8b34c6609016f5edcd3acc99ed7c9927fce7
Ozan Honamlıoğlu <ozanhonamlioglu@hotmail.com>
2021-05-13 03:21:02 +0300

    We have bridged up UIKit elements to SwiftUI world

--- Youtube/View/Pages/SearchView.swift
+++ Youtube/View/Pages/SearchView.swift
@@ -11,8 +11,12 @@ let searchPast = ["Test 1", "Test 2", "Test 3", "Test 4"]
 
 struct SearchView: View {
     @State var searchText: String = ""
+    @State var editingEnd: Bool = true {
+        didSet {
+            print(oldValue)
+        }
+    }
     @Binding var showSearch: Bool
-    
     
     var body: some View {
         ZStack {
@@ -24,7 +28,11 @@ struct SearchView: View {
                         showSearch = false
                     }, buttonImageName: "chevron.backward", activeButtonImageName: nil, active: nil, buttonLabel: nil)
                     
-                    TextField("Search", text: $searchText)
+                    // CustomTextfield(placeholder: "Search", text: $searchText, editingEnd: $editingEnd)
+                    CustomTextField(placeholder: "Search", text: $searchText) { status in
+                        print(status)
+                    }
+                    .frame(height: 30)
                     
                     SysImageButton(callback: nil, buttonImageName: "mic", activeButtonImageName: nil, active: nil, buttonLabel: nil)
                 }
@@ -38,11 +46,11 @@ struct SearchView: View {
                         ForEach(searchPast, id: \.self) { item in
                             
                             Button(action: {
-                                print("Do some bra")
+                                print("Just chill")
                             }, label: {
                                 EmptyView()
                             })
-                            .buttonStyle(StateableButton(change: { state in
+                            .buttonStyle(StateableButton { state in
                                 return HStack {
                                     Image(systemName: "clock.arrow.circlepath")
                                     Text(item)
@@ -51,9 +59,9 @@ struct SearchView: View {
                                 }
                                 .padding(.horizontal)
                                 .frame(height: 50)
-                                .background(state ? Color.black : Color.clear)
+                                .background(state ? Color("searchItemButton") : Color.clear)
                                 
-                            }))
+                            })
                             
                         }.foregroundColor(Color("generalForeground"))
                     }
@@ -75,3 +83,106 @@ struct SearchView_Previews: PreviewProvider {
         .preferredColorScheme(.dark)
     }
 }
+
+// This custom TextField implementation is only for this page
+final class MyTextFieldCoordinator: NSObject {
+    var control: CustomTextField
+
+    init(_ control: CustomTextField) {
+        self.control = control
+        super.init()
+        control.textField.addTarget(self, action: #selector(textFieldEditingDidBegin(_:)), for: .editingDidBegin)
+        control.textField.addTarget(self, action: #selector(textFieldEditingDidEnd(_:)), for: .editingDidEnd)
+        control.textField.addTarget(self, action: #selector(textFieldEditingChanged(_:)), for: .editingChanged)
+        control.textField.addTarget(self, action: #selector(textFieldEditingDidEndOnExit(_:)), for: .editingDidEndOnExit)
+        control.textField.autocorrectionType = .no
+        customToolBar()
+    }
+    
+    // setup custom toolbar
+    func customToolBar() {
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: control.textField.frame.width, height: 44))
+        let closeButton = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(closeAccessory))
+        let flexibleButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        toolbar.setItems([flexibleButton, closeButton], animated: true)
+        control.textField.inputAccessoryView = toolbar
+        control.textField.translatesAutoresizingMaskIntoConstraints = true
+    }
+
+    @objc private func textFieldEditingDidBegin(_ textField: UITextField) {
+        control.onEditingChanged(true)
+    }
+
+    @objc private func textFieldEditingDidEnd(_ textField: UITextField) {
+        control.onEditingChanged(false)
+    }
+
+    @objc private func textFieldEditingChanged(_ textField: UITextField) {
+        control.text = textField.text ?? ""
+    }
+
+    @objc private func textFieldEditingDidEndOnExit(_ textField: UITextField) {
+        control.onCommit()
+    }
+    
+    @objc func closeAccessory() {
+        control.textField.resignFirstResponder()
+    }
+}
+
+struct CustomTextField: UIViewRepresentable {
+
+    private let placeholder: String?
+    @Binding var text: String
+    let onEditingChanged: (Bool) -> Void
+    let onCommit: () -> Void
+    let onCancel: () -> Void
+
+    let textField = UITextField()
+
+    init(placeholder: String?, text: Binding<String>, onEditingChanged: @escaping (Bool) -> Void = { _ in }, onCommit: @escaping () -> Void = {}, onCancel: @escaping () -> Void = {}) {
+        self.placeholder = placeholder
+        self._text = text
+        self.onEditingChanged = onEditingChanged
+        self.onCommit = onCommit
+        self.onCancel = onCancel
+    }
+
+    func makeCoordinator() -> MyTextFieldCoordinator {
+        return MyTextFieldCoordinator(self)
+    }
+
+    func makeUIView(context: Context) -> UITextField {
+        textField.placeholder = placeholder
+        textField.delegate = context.coordinator as? UITextFieldDelegate
+        textField.returnKeyType = .done
+        textField.becomeFirstResponder()
+        return textField
+    }
+
+    func updateUIView(_ uiView: UITextField, context: Context) {
+    }
+}
+
+//struct CustomTextfield: UIViewRepresentable {
+//    var placeholder: String
+//    @Binding var text: String
+//    @Binding var editingEnd: Bool
+//
+//    var textfield: UITextField {
+//        let textfield = UITextField()
+//        textfield.placeholder = placeholder
+//        textfield.becomeFirstResponder()
+//        textfield.returnKeyType = .done
+//
+//        return textfield
+//    }
+//
+//    func makeUIView(context: Context) -> UITextField {
+//        return textfield
+//    }
+//
+//    func updateUIView(_ uiView: UITextField, context: Context) {
+//        uiView.text = text
+//    }
+//}
